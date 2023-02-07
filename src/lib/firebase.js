60f16173266181b4738303ae9802ccccd4b37727
Abdulrahman Mohammed <123945345+abdo286@users.noreply.github.com>
2023-02-07 13:25:15 +0200

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC-kJfo2JjZKDApYSEF-7vQWtYW3p3x2xM",
-  authDomain: "netflix-clone-f30c9.firebaseapp.com",
-  databaseURL: "https://netflix-clone-f30c9-default-rtdb.firebaseio.com",
-  projectId: "netflix-clone-f30c9",
-  storageBucket: "netflix-clone-f30c9.appspot.com",
-  messagingSenderId: "63579123276",
-  appId: "1:63579123276:web:26d8a9f7a8dac6c5e68ee9",
+  apiKey: "",
+  authDomain:"",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // init firebase app
